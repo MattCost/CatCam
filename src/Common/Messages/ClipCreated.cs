daa46eb816c07e79fd73159a8c8dee221ae44d17
Matt <matt.costuros@gmail.com>
2023-07-29 17:56:38 -0400

    add full path

--- src/Common/Messages/ClipCreated.cs
+++ src/Common/Messages/ClipCreated.cs
@@ -4,5 +4,6 @@ public class ClipCreated
 {
     public DateTime DateTime {get; set;} = DateTime.UtcNow;
     public string FileName {get;set;} = string.Empty;
+    public string FullPath {get;set;} = string.Empty;
 
 }
