445160eec340a879a7658a7f72504a8ce1010fa6
Matt <matt.costuros@gmail.com>
2023-07-30 09:22:17 -0400

    test file scripts

---
+++ scripts/create-test-file.sh
@@ -0,0 +1,5 @@
+delay=$(shuf -i 2-5 -n 1)
+filename=$RANDOM
+
+echo "file /home/matt/data/whatever/test-$filename over $delay seconds"
+flock --exclusive --wait 5 /home/matt/data/whatever/test-$filename sh -c "echo hello world > /home/matt/data/whatever/test-$filename ; sleep $delay ; echo goodbye >> /home/matt/data/whatever/test-$filename"
---
+++ scripts/generate-files.sh
@@ -0,0 +1,14 @@
+count=$(shuf -i 5-10 -n 1)
+
+echo Generating $count files with delays
+
+for (( i=0 ; i<$count; i++))
+do
+    # echo creating file iteration $i
+    sh -c ./create-test-file.sh
+done
+
+echo Cleaning up files
+cd ~/data/whatever
+# pwd
+rm test*
