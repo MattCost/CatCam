19bd2dcd5f82f0938b32b33173a0e82fc9e69f23
Matt <matt.costuros@gmail.com>
2023-08-05 09:58:17 -0400

    local builds are tagged as such

--- scripts/build-file-watcher.sh
+++ scripts/build-file-watcher.sh
@@ -4,8 +4,8 @@ patch=$(cat src/EdgeModules/FileWatcher/patch.txt)
 echo "patch $patch"
 echo $patch > src/EdgeModules/FileWatcher/patch.txt
 TOKEN=$(az acr login --name catcamacr --expose-token --output tsv --query accessToken)
-docker build --tag catcamacr.azurecr.io/file-watcher:0.0.$patch-linux-amd64 -f EdgeModules/FileWatcher/Dockerfile.amd64 src
-docker build --tag catcamacr.azurecr.io/file-watcher:0.0.$patch-linux-arm32v7 -f EdgeModules/FileWatcher/Dockerfile.arm32v7 src
+docker build --tag catcamacr.azurecr.io/file-watcher:0.0.$patch-local-amd64 -f EdgeModules/FileWatcher/Dockerfile.amd64 src
+docker build --tag catcamacr.azurecr.io/file-watcher:0.0.$patch-local-arm32v7 -f EdgeModules/FileWatcher/Dockerfile.arm32v7 src
 docker login catcamacr.azurecr.io --username 00000000-0000-0000-0000-000000000000 --password-stdin <<< $TOKEN
-docker push catcamacr.azurecr.io/file-watcher:0.0.$patch-linux-amd64
-docker push catcamacr.azurecr.io/file-watcher:0.0.$patch-linux-arm32v7
+docker push catcamacr.azurecr.io/file-watcher:0.0.$patch-local-amd64
+docker push catcamacr.azurecr.io/file-watcher:0.0.$patch-local-arm32v7
--- scripts/build-video-capture.sh
+++ scripts/build-video-capture.sh
@@ -4,8 +4,8 @@ patch=$(cat src/EdgeModules/VideoCapture/patch.txt)
 echo "patch $patch"
 echo $patch > src/EdgeModules/VideoCapture/patch.txt
 TOKEN=$(az acr login --name catcamacr --expose-token --output tsv --query accessToken)
-docker build --tag catcamacr.azurecr.io/video-capture:0.0.$patch-linux-amd64 -f Dockerfile.amd64 src/EdgeModules/VideoCapture/
-docker build --tag catcamacr.azurecr.io/video-capture:0.0.$patch-linux-arm32v7 -f Dockerfile.arm32v7 src/EdgeModules/VideoCapture/
+docker build --tag catcamacr.azurecr.io/video-capture:0.0.$patch-local-amd64 -f Dockerfile.amd64 src/EdgeModules/VideoCapture/
+docker build --tag catcamacr.azurecr.io/video-capture:0.0.$patch-local-arm32v7 -f Dockerfile.arm32v7 src/EdgeModules/VideoCapture/
 docker login catcamacr.azurecr.io --username 00000000-0000-0000-0000-000000000000 --password-stdin <<< $TOKEN
-docker push catcamacr.azurecr.io/video-capture:0.0.$patch-linux-amd64
-docker push catcamacr.azurecr.io/video-capture:0.0.$patch-linux-arm32v7
+docker push catcamacr.azurecr.io/video-capture:0.0.$patch-local-amd64
+docker push catcamacr.azurecr.io/video-capture:0.0.$patch-local-arm32v7
