4faa97630b9e70e2210c0309754f2408d3ad24f3
Matt <matt.costuros@gmail.com>
2023-08-06 10:40:53 -0400

    moved to edgeCommon

--- src/Common/Messages/ClipCreated.cs
+++
@@ -1,9 +0,0 @@
-namespace CatCam.Common.Messages;
-
-public class ClipCreated
-{
-    public DateTime DateTime {get; set;} = DateTime.UtcNow;
-    public string FileName {get;set;} = string.Empty;
-    public string FullPath {get;set;} = string.Empty;
-
-}
